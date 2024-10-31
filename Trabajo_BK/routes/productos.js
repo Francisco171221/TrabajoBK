c8d868184db8242385b0e83f56c60f878f2699c4
Francisco <2120100403@soy.utj.edu.mx>
2024-10-30 20:15:40 -0600

    termino producto

--- Trabajo_BK/routes/productos.js
+++ Trabajo_BK/routes/productos.js
@@ -95,9 +95,57 @@ router.put('/imagen',upload.single('imagen'),async(req,res)=>{
         {
             new:true
         },
+
     );
 
     await fs.unlink(path.resolve("almacen/img/"+urlfotoanterior[4]));
 
     res.send({prod_modificado});
 });
+
+router.delete('/borrar/:nombre', async (req,res)=>{
+    let prod = await Producto.findOne({nombre:req.params.nombre});
+    if(!prod){
+        return res.status(402).send("Producto no encontrado");
+    }
+
+    /*http://localhost:3000/foto/imagen-1636379151090-59215371.png */
+    let urlfotoanterior = prod.imgurl.split("/");
+    //console.log(urlfotoanterior[4]);
+    //obtiene la url de la imagen almacenada
+    //agregar a imgurl dicha url obtenida
+
+    let prod_eliminada = await Producto.findOneAndDelete({nombre:req.params.nombre});
+
+    await fs.unlink(path.resolve("almacen/img/"+urlfotoanterior[4]));
+
+    res.send({prod_eliminada});
+});
+
+router.get('/', async(req,res)=>{
+    /* try{
+            cont resultado = await Producto.find();
+             res.json(resultado);
+    }catch(err){
+            console.error(err);
+            res.status(500).send('Error en el servidor);
+    } */
+   let pro = await Producto.find({});
+   res.send(pro);
+});
+
+router.get('/nombre/:nombre', async(req,res)=>{
+    let pro = await Producto.findOne({nombre:req.params.nombre});
+
+    if(!pro){
+        return res.status(401).send("Producto no encontrado");
+    }
+
+    //envia toda la informacion
+    res.send({pro});
+
+    //envio de objeto personalizado
+    //res.send({emp_envio});
+});
+
+module.exports = router;
